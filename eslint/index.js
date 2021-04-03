8bca3cb9c448dab436b37f95bc5cc635135a2eb1
Tyler Lawson <tylerjlawson2@gmail.com>
2021-04-03 13:48:32 -0400

    first commit

---
+++ eslint/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: "eslint:recommended",
+  parser: "babel-eslint",
+  env: {
+    node: true,
+    "jest/globals": true,
+    browser: true,
+    es6: true,
+  },
+  plugins: ["jest"],
+};
